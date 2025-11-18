36daed5155a5059697a56e97d1e0532af550c58d
drskorokhod-boop <dr.skorokhod@yandex.ru>
2025-11-18 12:00:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1285,7 +1285,7 @@ if media_files:
 else:
     # Если медиа нет — вставляем логотип по умолчанию
     try:
-        with open("assets/banner_default.jpg", "rb") as img:
+        with open("assets/imgonline-com-ua-Resize-poVtNXt7aue6.png", "rb") as img:
             sent = await m.bot.send_photo(m.chat.id, img, caption="")
             b_media = {"type": "photo", "file_id": sent.photo[-1].file_id}
             await m.bot.delete_message(m.chat.id, sent.message_id)
